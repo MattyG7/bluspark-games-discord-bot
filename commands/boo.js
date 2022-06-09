19ff4fef966fb451dfd5cbfbdb21505e2058ecd9
MattyG7 <ml.greaves1@btinternet.com>
2022-06-10 00:45:05 +0100

    Update boo.js

--- commands/boo.js
+++ commands/boo.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const mongoose = require(`mongoose`);
-const Schema = mongoose.Schema;
 
 module.exports.run = async (bot, message, args) => {
   let rUser = message.mentions.members.first();
@@ -15,30 +14,22 @@ module.exports.run = async (bot, message, args) => {
       console.log("Failed to get data :(");
       console.log(error);
     } else {
-      let userColour = "";
-      if (data.col === "not-set") {
-        userColour = "202225";
-      } else {
-        userColour = data.col;
-      }
-      console.log("Got user's colour Successfully!");
-
       let replies = ["0", "1"];
       let result = Math.floor((Math.random() * replies.length));
 
       if(result === 0) {
-        let booicon = message.author.displayAvatarURL;
-        let booembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let booicon = message.author.displayAvatarURL();
+        let booembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Boo 👻", booicon)
         .setDescription(`***${message.author.username} made ${rUser.user.username} jump!***\n*[Screams]* 😱`)
         .setImage (`https://blusparkstudio.com/discord/boo_1.gif`);
         return message.channel.send(booembed);
       }
       if(result === 1) {
-        let booicon = message.author.displayAvatarURL;
-        let booembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let booicon = message.author.displayAvatarURL();
+        let booembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Boo 👻", booicon)
         .setDescription(`***${message.author.username} made ${rUser.user.username} jump!***\n*[Screams]* 😱`)
         .setImage (`https://blusparkstudio.com/discord/boo_2.gif`);
