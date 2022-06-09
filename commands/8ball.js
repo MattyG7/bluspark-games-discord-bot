6c8a54ec309ff01cbf585897e1ef6893323a0cd8
MattyG7 <ml.greaves1@btinternet.com>
2022-06-10 00:41:41 +0100

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const mongoose = require(`mongoose`);
 
 module.exports.run = async (bot, message, args) => {
   mongoose.model("DiscordUserData").findOne ({
@@ -9,13 +10,13 @@ module.exports.run = async (bot, message, args) => {
       console.log(error);
     } else {
       if(!args[2]) return message.channel.send(`${message.author.username}, please ask a full question.`);
-    
+
       let replies = ["Yes.", "My magic failed to work, try again later.", "It's better if you don't know the answer right now.", "Ask someone else, I'm busy right now.", "Probably not.", "Definitely.", "Maybe.", "No.", "Definitely not.", "Yes, definitely.", "I don't know.", "Let me think about it..."];
       let result = Math.floor((Math.random() * replies.length));
-    
+
       if (result === 11) {
         let bllembed = new Discord.MessageEmbed()
-        .setColor(data.col)
+        .setColor(`${data.col}`)
         .setTitle(`*8-Ball*`)
         .setDescription(`**${replies[result]}**`);
         message.channel.send(bllembed);
@@ -23,14 +24,14 @@ module.exports.run = async (bot, message, args) => {
         let resultMore = Math.floor((Math.random() * repliesMore.length));
         setTimeout(() => {
           let blllembed = new Discord.MessageEmbed()
-          .setColor(data.col)
+          .setColor(`${data.col}`)
           .setDescription(`**${repliesMore[resultMore]}**`);
           return message.channel.send(blllembed);
         }, 3000);
       }
       if (result != 11) {
         let bllembed = new Discord.MessageEmbed()
-        .setColor(data.col)
+        .setColor(`${data.col}`)
         .setTitle(`*8-Ball*`)
         .setDescription(`**${replies[result]}**`);
         return message.channel.send(bllembed);
