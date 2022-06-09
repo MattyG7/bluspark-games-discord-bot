61bdabb40acadc371b1bf864f441a81b465d7312
MattyG7 <ml.greaves1@btinternet.com>
2022-06-10 00:46:44 +0100

    Update bye.js

--- commands/bye.js
+++ commands/bye.js
@@ -5,25 +5,25 @@ module.exports.run = async (bot, message, args) => {
   let result = Math.floor((Math.random() * replies.length));
 
   if(result === 0) {
-    return message.channel.send(`Bye ${message.author.username}`);
+    return message.channel.send(`Bye ${message.author.username}.`);
   }
   if(result === 1) {
-    return message.channel.send(`Goodbye ${message.author.username}`);
+    return message.channel.send(`Goodbye ${message.author.username}.`);
   }
   if(result === 2) {
-    return message.channel.send(`See ya ${message.author.username}`);
+    return message.channel.send(`See ya ${message.author.username}.`);
   }
   if(result === 3) {
-    return message.channel.send(`See you later ${message.author.username}`);
+    return message.channel.send(`See you later ${message.author.username}.`);
   }
   if(result === 4) {
-    return message.channel.send(`Laterz ${message.author.username}`);
+    return message.channel.send(`Laterz ${message.author.username}.`);
   }
   if(result === 5) {
-    return message.channel.send(`So long ${message.author.username}`);
+    return message.channel.send(`So long ${message.author.username}.`);
   }
   if(result === 6) {
-    return message.channel.send(`Till the next time ${message.author.username}`);
+    return message.channel.send(`Till the next time ${message.author.username}.`);
   }
   if(result === 7) {
     return message.channel.send(`😴 ...\n[Automatic message] Sorry, I am asleep right now. Yes, bots need sleep too.`);
