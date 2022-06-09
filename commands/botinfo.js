ae9a17bb22fdd43a4ac53fa252081e25b43eabe0
MattyG7 <ml.greaves1@btinternet.com>
2022-06-10 00:45:51 +0100

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, author) => {
-  let bicon = bot.user.displayAvatarURL;
-    let botembed = new Discord.RichEmbed()
+  let bicon = bot.user.displayAvatarURL();
+    let botembed = new Discord.MessageEmbed()
     .setDescription("**Bot Info:**")
-    .setColor("#827ec8")
+    .setColor("#7c889c")
     .setThumbnail(bicon)
     .addField("Bot Name", bot.user.username)
     .addField("Date Created", bot.user.createdAt)
