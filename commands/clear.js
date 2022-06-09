ba26239aa7a8ae43f0904d2de4847acb67e322df
MattyG7 <ml.greaves1@btinternet.com>
2022-06-10 00:48:33 +0100

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -5,19 +5,15 @@ module.exports.run = async (bot, message, args, author) => {
   if (!args[0]) return message.channel.send("How many messages would you like to clear? ~clear **NUMBER**.");
   if (isNaN(args[0])) return message.channel.send("Please use a number.");
   let numParsedArgs = parseInt(args[0]);
-  if (numParsedArgs > 25) return message.channel.send("That is too many! Try 25.");
+  if (numParsedArgs > 10) return message.channel.send("That is too many! Try 10."); //25
   if (message.member.user.tag === author) {
     if (numParsedArgs === 1) {
       message.channel.bulkDelete(2);
-      message.channel.send(`1 message cleared.`).then(msg => {
-        msg.delete(5000)
-      });
+      message.channel.send(`1 message cleared.`).then(msg => {setTimeout(() => msg.delete(), 5000)});
     } else {
       let numParsedArgsPLUSONE = numParsedArgs + 1;
       message.channel.bulkDelete(numParsedArgsPLUSONE);
-      message.channel.send(`${numParsedArgs} messages cleared.`).then(msg => {
-        msg.delete(5000)
-      });
+      message.channel.send(`${numParsedArgs} messages cleared.`).then(msg => {setTimeout(() => msg.delete(), 5000)});
     }
   }
 }
