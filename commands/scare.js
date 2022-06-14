336ad59686e034ad2a5f5400ce2775f448bd5475
MattyG7 <ml.greaves1@btinternet.com>
2022-06-15 00:48:40 +0100

    Update scare.js

--- commands/scare.js
+++ commands/scare.js
@@ -1,13 +1,11 @@
 const Discord = require("discord.js");
 const mongoose = require(`mongoose`);
-const Schema = mongoose.Schema;
 
 module.exports.run = async (bot, message, args) => {
   let rUser = message.mentions.members.first();
   if(!rUser) return message.channel.send(`User not found! 😕`);
   if(rUser.id === message.author.id) return message.channel.send(`***${message.author.username} just scared themself. Feel free to laugh.***`);
 
-  //GET USER COLOUR
   mongoose.model("DiscordUserData").findOne ({
     userID: `${message.author.id}`
   }, function(error, data) {
@@ -15,48 +13,40 @@ module.exports.run = async (bot, message, args) => {
       console.log("Failed to get data :(");
       console.log(error);
     } else {
-      let userColour = "";
-      if (data.col === "not-set") {
-        userColour = "202225";
-      } else {
-        userColour = data.col;
-      }
-      console.log("Got user's colour Successfully!");
-
       let replies = ["0", "1", "2", "3"];
       let result = Math.floor((Math.random() * replies.length));
 
       if(result === 0) {
-        let scricon = message.author.displayAvatarURL;
-        let scrembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let scricon = message.author.displayAvatarURL();
+        let scrembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Scare 👻", scricon)
         .setDescription(`***${message.author.username} made ${rUser.user.username} jump!***\n*[Screams]* 😱`)
         .setImage (`https://blusparkstudio.com/discord/boo_1.gif`);
         return message.channel.send(scrembed);
       }
       if(result === 1) {
-        let scricon = message.author.displayAvatarURL;
-        let scrembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let scricon = message.author.displayAvatarURL();
+        let scrembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Scare 👻", scricon)
         .setDescription(`***${message.author.username} made ${rUser.user.username} jump!***\n*[Screams]* 😱`)
         .setImage (`https://blusparkstudio.com/discord/boo_2.gif`);
         return message.channel.send(scrembed);
       }
       if(result === 2) {
-        let scricon = message.author.displayAvatarURL;
-        let scrembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let scricon = message.author.displayAvatarURL();
+        let scrembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Scare 👻", scricon)
         .setDescription(`***${message.author.username} made ${rUser.user.username} jump!***\n*[Screams]* 😱`)
         .setImage (`https://blusparkstudio.com/discord/poke_3.gif`);
         return message.channel.send(scrembed);
       }
       if(result === 3) {
-        let scricon = message.author.displayAvatarURL;
-        let scrembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let scricon = message.author.displayAvatarURL();
+        let scrembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Scare 👻", scricon)
         .setDescription(`***${message.author.username} made ${rUser.user.username} jump!***\n*[Screams]* 😱`)
         .setImage (`https://blusparkstudio.com/discord/poke_13.gif`);
