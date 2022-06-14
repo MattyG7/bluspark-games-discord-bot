212b30256f526e20334f86255fa08e44ce5ec215
MattyG7 <ml.greaves1@btinternet.com>
2022-06-15 00:24:42 +0100

    Update eat.js

--- commands/eat.js
+++ commands/eat.js
@@ -1,9 +1,7 @@
 const Discord = require("discord.js");
 const mongoose = require(`mongoose`);
-const Schema = mongoose.Schema;
 
 module.exports.run = async (bot, message, args) => {
-  //GET USER COLOUR
   mongoose.model("DiscordUserData").findOne ({
     userID: `${message.author.id}`
   }, function(error, data) {
@@ -11,102 +9,94 @@ module.exports.run = async (bot, message, args) => {
       console.log("Failed to get data :(");
       console.log(error);
     } else {
-      let userColour = "";
-      if (data.col === "not-set") {
-        userColour = "202225";
-      } else {
-        userColour = data.col;
-      }
-      console.log("Got user's colour Successfully!");
-
       if (!args[0]) {
         let replies = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
         let result = Math.floor((Math.random() * replies.length));
 
         if(result === 0) {
-          let eticon = message.author.displayAvatarURL;
-          let etembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let eticon = message.author.displayAvatarURL();
+          let etembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is eating.`, eticon)
           .setImage (`https://blusparkstudio.com/discord/eat_1.gif`);
           return message.channel.send(etembed);
         }
         if(result === 1) {
-          let eticon = message.author.displayAvatarURL;
-          let etembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let eticon = message.author.displayAvatarURL();
+          let etembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is eating.`, eticon)
           .setImage (`https://blusparkstudio.com/discord/eat_2.gif`);
           return message.channel.send(etembed);
         }
         if(result === 2) {
-          let eticon = message.author.displayAvatarURL;
-          let etembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let eticon = message.author.displayAvatarURL();
+          let etembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is eating.`, eticon)
           .setImage (`https://blusparkstudio.com/discord/eat_3.gif`);
           return message.channel.send(etembed);
         }
         if(result === 3) {
-          let eticon = message.author.displayAvatarURL;
-          let etembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let eticon = message.author.displayAvatarURL();
+          let etembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is eating.`, eticon)
           .setImage (`https://blusparkstudio.com/discord/eat_4.gif`);
           return message.channel.send(etembed);
         }
         if(result === 4) {
-          let eticon = message.author.displayAvatarURL;
-          let etembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let eticon = message.author.displayAvatarURL();
+          let etembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is eating.`, eticon)
           .setImage (`https://blusparkstudio.com/discord/eat_5.gif`);
           return message.channel.send(etembed);
         }
         if(result === 5) {
-          let eticon = message.author.displayAvatarURL;
-          let etembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let eticon = message.author.displayAvatarURL();
+          let etembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is eating.`, eticon)
           .setImage (`https://blusparkstudio.com/discord/eat_6.gif`);
           return message.channel.send(etembed);
         }
         if(result === 6) {
-          let eticon = message.author.displayAvatarURL;
-          let etembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let eticon = message.author.displayAvatarURL();
+          let etembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is eating.`, eticon)
           .setImage (`https://blusparkstudio.com/discord/eat_7.gif`);
           return message.channel.send(etembed);
         }
         if(result === 7) {
-          let eticon = message.author.displayAvatarURL;
-          let etembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let eticon = message.author.displayAvatarURL();
+          let etembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is eating.`, eticon)
           .setImage (`https://blusparkstudio.com/discord/eat_8.gif`);
           return message.channel.send(etembed);
         }
         if(result === 8) {
-          let eticon = message.author.displayAvatarURL;
-          let etembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let eticon = message.author.displayAvatarURL();
+          let etembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is eating.`, eticon)
           .setImage (`https://blusparkstudio.com/discord/eat_9.gif`);
           return message.channel.send(etembed);
         }
         if(result === 9) {
-          let eticon = message.author.displayAvatarURL;
-          let etembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let eticon = message.author.displayAvatarURL();
+          let etembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is eating.`, eticon)
           .setImage (`https://blusparkstudio.com/discord/eat_10.gif`);
           return message.channel.send(etembed);
         }
         if(result === 10) {
-          let eticon = message.author.displayAvatarURL;
-          let etembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let eticon = message.author.displayAvatarURL();
+          let etembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} tried to cook something to eat...`, eticon)
           .setImage (`https://blusparkstudio.com/discord/eat_11.gif`);
           return message.channel.send(etembed);
@@ -120,99 +110,99 @@ module.exports.run = async (bot, message, args) => {
       let result = Math.floor((Math.random() * replies.length));
 
       if(result === 0) {
-        let eticon = message.author.displayAvatarURL;
-        let etembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let eticon = message.author.displayAvatarURL();
+        let etembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Eat 🍴", eticon)
         .setDescription(`***${message.author.username} ate ${rUser.user.username}.***\n😦`)
         .setImage (`https://blusparkstudio.com/discord/eat_1.gif`);
         return message.channel.send(etembed);
       }
       if(result === 1) {
-        let eticon = message.author.displayAvatarURL;
-        let etembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let eticon = message.author.displayAvatarURL();
+        let etembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Eat 🍴", eticon)
         .setDescription(`***${message.author.username} ate ${rUser.user.username}.***\n😦`)
         .setImage (`https://blusparkstudio.com/discord/eat_2.gif`);
         return message.channel.send(etembed);
       }
       if(result === 2) {
-        let eticon = message.author.displayAvatarURL;
-        let etembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let eticon = message.author.displayAvatarURL();
+        let etembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Eat 🍴", eticon)
         .setDescription(`***${message.author.username} ate ${rUser.user.username}.***\n😦`)
         .setImage (`https://blusparkstudio.com/discord/eat_3.gif`);
         return message.channel.send(etembed);
       }
       if(result === 3) {
-        let eticon = message.author.displayAvatarURL;
-        let etembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let eticon = message.author.displayAvatarURL();
+        let etembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Eat 🍴", eticon)
         .setDescription(`***${message.author.username} ate ${rUser.user.username}.***\n😦`)
         .setImage (`https://blusparkstudio.com/discord/eat_4.gif`);
         return message.channel.send(etembed);
       }
       if(result === 4) {
-        let eticon = message.author.displayAvatarURL;
-        let etembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let eticon = message.author.displayAvatarURL();
+        let etembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Eat 🍴", eticon)
         .setDescription(`***${message.author.username} ate ${rUser.user.username}.***\n😦`)
         .setImage (`https://blusparkstudio.com/discord/eat_5.gif`);
         return message.channel.send(etembed);
       }
       if(result === 5) {
-        let eticon = message.author.displayAvatarURL;
-        let etembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let eticon = message.author.displayAvatarURL();
+        let etembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Eat 🍴", eticon)
         .setDescription(`***${message.author.username} ate ${rUser.user.username}.***\n😦`)
         .setImage (`https://blusparkstudio.com/discord/eat_6.gif`);
         return message.channel.send(etembed);
       }
       if(result === 6) {
-        let eticon = message.author.displayAvatarURL;
-        let etembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let eticon = message.author.displayAvatarURL();
+        let etembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Eat 🍴", eticon)
         .setDescription(`***${message.author.username} ate ${rUser.user.username}.***\n😦`)
         .setImage (`https://blusparkstudio.com/discord/eat_7.gif`);
         return message.channel.send(etembed);
       }
       if(result === 7) {
-        let eticon = message.author.displayAvatarURL;
-        let etembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let eticon = message.author.displayAvatarURL();
+        let etembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Eat 🍴", eticon)
         .setDescription(`***${message.author.username} ate ${rUser.user.username}.***\n😦`)
         .setImage (`https://blusparkstudio.com/discord/eat_8.gif`);
         return message.channel.send(etembed);
       }
       if(result === 8) {
-        let eticon = message.author.displayAvatarURL;
-        let etembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let eticon = message.author.displayAvatarURL();
+        let etembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Eat 🍴", eticon)
         .setDescription(`***${message.author.username} ate ${rUser.user.username}.***\n😦`)
         .setImage (`https://blusparkstudio.com/discord/eat_9.gif`);
         return message.channel.send(etembed);
       }
       if(result === 9) {
-        let eticon = message.author.displayAvatarURL;
-        let etembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let eticon = message.author.displayAvatarURL();
+        let etembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Eat 🍴", eticon)
         .setDescription(`***${message.author.username} ate ${rUser.user.username}.***\n😦`)
         .setImage (`https://blusparkstudio.com/discord/eat_10.gif`);
         return message.channel.send(etembed);
       }
       if(result === 10) {
-        let eticon = message.author.displayAvatarURL;
-        let etembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let eticon = message.author.displayAvatarURL();
+        let etembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Eat 🍴", eticon)
         .setDescription(`***${message.author.username} tried to cook wiht ${rUser.user.username}.***\n😦`)
         .setImage (`https://blusparkstudio.com/discord/eat_11.gif`);
