9505354f1ad79591d9c8993a3732269dd55d3f62
MattyG7 <ml.greaves1@btinternet.com>
2022-06-15 00:48:30 +0100

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -4,15 +4,15 @@ module.exports.run = async (bot, message, args) => {
   let sicon = message.guild.iconURL;
   let memberCount = parseInt(message.guild.memberCount);
   memberCount = memberCount - 3;
-    let serverembed = new Discord.RichEmbed()
+    let serverembed = new Discord.MessageEmbed()
     .setDescription("**Server Info:**")
-    .setColor("#667ed6")
+    .setColor("#627bd5")
     .setThumbnail(sicon)
     .addField("Server Name", message.guild.name)
     .addField("Date Created", message.guild.createdAt)
     .addField("You Joined", message.member.joinedAt)
     .addField("Member Count", memberCount, true)
-    .addField("Total Members (including bots and me)", message.guild.memberCount, true);
+    .addField("Total Members (including bots)", message.guild.memberCount, true);
     return message.channel.send(serverembed);
 }
 
