dc2b2ca70bd141164784a3f6fdd16503207d2c04
MattyG7 <ml.greaves1@btinternet.com>
2022-06-15 00:45:09 +0100

    Update punch.js

--- commands/punch.js
+++ commands/punch.js
@@ -1,13 +1,11 @@
 const Discord = require("discord.js");
 const mongoose = require(`mongoose`);
-const Schema = mongoose.Schema;
 
 module.exports.run = async (bot, message, args) => {
   let rUser = message.mentions.members.first();
   if(!rUser) return message.channel.send(`User not found! 😕`);
   if(rUser.id === message.author.id) return message.channel.send(`***No! Don't hurt yourself, ${message.author.username}.***`);
 
-  //GET USER COLOUR
   mongoose.model("DiscordUserData").findOne ({
     userID: `${message.author.id}`
   }, function(error, data) {
@@ -15,66 +13,58 @@ module.exports.run = async (bot, message, args) => {
       console.log("Failed to get data :(");
       console.log(error);
     } else {
-      let userColour = "";
-      if (data.col === "not-set") {
-        userColour = "202225";
-      } else {
-        userColour = data.col;
-      }
-      console.log("Got user's colour Successfully!");
-
       let replies = ["0", "1", "2", "3", "4", "5"];
       let result = Math.floor((Math.random() * replies.length));
 
       if(result === 0) {
-        let pnchicon = message.author.displayAvatarURL;
-        let pnchembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let pnchicon = message.author.displayAvatarURL();
+        let pnchembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Punch 👊", pnchicon)
         .setDescription(`***${message.author.username} punched ${rUser.user.username}!***\nOuch...I bet that hurt. 😬`)
         .setImage (`https://blusparkstudio.com/discord/punch_1.gif`);
         return message.channel.send(pnchembed);
       }
       if(result === 1) {
-        let pnchicon = message.author.displayAvatarURL;
-        let pnchembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let pnchicon = message.author.displayAvatarURL();
+        let pnchembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Punch 👊", pnchicon)
         .setDescription(`***${message.author.username} punched ${rUser.user.username}!***\nOuch...I bet that hurt. 😬`)
         .setImage (`https://blusparkstudio.com/discord/punch_2.gif`);
         return message.channel.send(pnchembed);
       }
       if(result === 2) {
-        let pnchicon = message.author.displayAvatarURL;
-        let pnchembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let pnchicon = message.author.displayAvatarURL();
+        let pnchembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Punch 👊", pnchicon)
         .setDescription(`***${message.author.username} punched ${rUser.user.username}!***\nOuch...I bet that hurt. 😬`)
         .setImage (`https://blusparkstudio.com/discord/punch_3.gif`);
         return message.channel.send(pnchembed);
       }
       if(result === 3) {
-        let pnchicon = message.author.displayAvatarURL;
-        let pnchembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let pnchicon = message.author.displayAvatarURL();
+        let pnchembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Punch 👊", pnchicon)
         .setDescription(`***${message.author.username} is punching ${rUser.user.username}!***\nOuch...That looks like it hurts. 😬`)
         .setImage (`https://blusparkstudio.com/discord/punch_4.gif`);
         return message.channel.send(pnchembed);
       }
       if(result === 4) {
-        let pnchicon = message.author.displayAvatarURL;
-        let pnchembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let pnchicon = message.author.displayAvatarURL();
+        let pnchembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Punch 👊", pnchicon)
         .setDescription(`***${message.author.username} punched ${rUser.user.username}!***\nOuch...I bet that hurt. 😬`)
         .setImage (`https://blusparkstudio.com/discord/punch_5.gif`);
         return message.channel.send(pnchembed);
       }
       if(result === 5) {
-        let pnchicon = message.author.displayAvatarURL;
-        let pnchembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let pnchicon = message.author.displayAvatarURL();
+        let pnchembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Punch 👊", pnchicon)
         .setDescription(`***${message.author.username} is punching ${rUser.user.username} very fast!***\nOuch...😬`)
         .setImage (`https://blusparkstudio.com/discord/punch_6.gif`);
