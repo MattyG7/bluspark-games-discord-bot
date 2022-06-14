28a31d36448227354c952b69c0eca86a70b70865
MattyG7 <ml.greaves1@btinternet.com>
2022-06-15 00:38:35 +0100

    Update log.js

--- commands/log.js
+++ commands/log.js
@@ -2,11 +2,11 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, author) => {
   if (message.member.user.tag !== author) return message.channel.send("🚫 You're not allowed to use this command!");
-  let sparkcoinlogmembed = new Discord.RichEmbed()
-  .setColor("#1c9472")
+  let sparkcoinlogmembed = new Discord.MessageEmbed()
+  .setColor("#7c889c")
   .setDescription(`**${args[0]}** won ${args[1]} SparkCoins.`)
   .setFooter(`${args[2]}`);
-  bot.channels.get(`681249230232223767`).send(sparkcoinlogmembed);
+  bot.channels.cache.get(`681249230232223767`).send(sparkcoinlogmembed);
 }
 
 module.exports.help = {
