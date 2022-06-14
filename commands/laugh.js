ff4bf6aa7b343872948bcdb6109d57802a2c5cae
MattyG7 <ml.greaves1@btinternet.com>
2022-06-15 00:37:06 +0100

    Update laugh.js

--- commands/laugh.js
+++ commands/laugh.js
@@ -1,9 +1,7 @@
 const Discord = require("discord.js");
 const mongoose = require(`mongoose`);
-const Schema = mongoose.Schema;
 
 module.exports.run = async (bot, message, args) => {
-  //GET USER COLOUR
   mongoose.model("DiscordUserData").findOne ({
     userID: `${message.author.id}`
   }, function(error, data) {
@@ -11,86 +9,78 @@ module.exports.run = async (bot, message, args) => {
       console.log("Failed to get data :(");
       console.log(error);
     } else {
-      let userColour = "";
-      if (data.col === "not-set") {
-        userColour = "202225";
-      } else {
-        userColour = data.col;
-      }
-      console.log("Got user's colour Successfully!");
-
       if (!args[0]) {
         let replies = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
         let result = Math.floor((Math.random() * replies.length));
 
         if(result === 0) {
-          let lghicon = message.author.displayAvatarURL;
-          let lghembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let lghicon = message.author.displayAvatarURL();
+          let lghembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} finds that very funny!`, lghicon)
           .setImage (`https://blusparkstudio.com/discord/laugh_1.gif`);
           return message.channel.send(lghembed);
         }
         if(result === 1) {
-          let lghicon = message.author.displayAvatarURL;
-          let lghembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let lghicon = message.author.displayAvatarURL();
+          let lghembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} finds that quite funny!`, lghicon)
           .setImage (`https://blusparkstudio.com/discord/laugh_2.gif`);
           return message.channel.send(lghembed);
         }
         if(result === 2) {
-          let lghicon = message.author.displayAvatarURL;
-          let lghembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let lghicon = message.author.displayAvatarURL();
+          let lghembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} finds that funny!`, lghicon)
           .setImage (`https://blusparkstudio.com/discord/laugh_3.gif`);
           return message.channel.send(lghembed);
         }
         if(result === 3) {
-          let lghicon = message.author.displayAvatarURL;
-          let lghembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let lghicon = message.author.displayAvatarURL();
+          let lghembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} finds that funny!`, lghicon)
           .setImage (`https://blusparkstudio.com/discord/laugh_4.gif`);
           return message.channel.send(lghembed);
         }
         if(result === 4) {
-          let lghicon = message.author.displayAvatarURL;
-          let lghembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let lghicon = message.author.displayAvatarURL();
+          let lghembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} finds that pretty funny!`, lghicon)
           .setImage (`https://blusparkstudio.com/discord/laugh_5.gif`);
           return message.channel.send(lghembed);
         }
         if(result === 5) {
-          let lghicon = message.author.displayAvatarURL;
-          let lghembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let lghicon = message.author.displayAvatarURL();
+          let lghembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is doing an evil laugh...`, lghicon)
           .setImage (`https://blusparkstudio.com/discord/laugh_6.gif`);
           return message.channel.send(lghembed);
         }
         if(result === 6) {
-          let lghicon = message.author.displayAvatarURL;
-          let lghembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let lghicon = message.author.displayAvatarURL();
+          let lghembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} finds that funny!`, lghicon)
           .setImage (`https://blusparkstudio.com/discord/laugh_7.gif`);
           return message.channel.send(lghembed);
         }
         if(result === 7) {
-          let lghicon = message.author.displayAvatarURL;
-          let lghembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let lghicon = message.author.displayAvatarURL();
+          let lghembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} finds that funny!`, lghicon)
           .setImage (`https://blusparkstudio.com/discord/laugh_8.gif`);
           return message.channel.send(lghembed);
         }
         if(result === 8) {
-          let lghicon = message.author.displayAvatarURL;
-          let lghembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let lghicon = message.author.displayAvatarURL();
+          let lghembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} finds that funny!`, lghicon)
           .setImage (`https://blusparkstudio.com/discord/laugh_9.gif`);
           return message.channel.send(lghembed);
@@ -98,87 +88,87 @@ module.exports.run = async (bot, message, args) => {
       }
       let rUser = message.mentions.members.first();
       if(args[0] && !rUser) return message.channel.send(`User not found! 😕`);
-      if(rUser.id === message.author.id) return message.channel.send(`***${message.author.username} is laughing at themself. Everyone, back away slowly...***`);
+      if(rUser.id === message.author.id) return message.channel.send(`***${message.author.username} is laughing at themself...***`);
 
       let replies = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
       let result = Math.floor((Math.random() * replies.length));
 
       if(result === 0) {
-        let lghicon = message.author.displayAvatarURL;
-        let lghembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let lghicon = message.author.displayAvatarURL();
+        let lghembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Laugh 😂", lghicon)
         .setDescription(`***${message.author.username} is laughing at ${rUser.user.username}.***\nWhat's so funny? 😅`)
         .setImage (`https://blusparkstudio.com/discord/laugh_1.gif`);
         return message.channel.send(lghembed);
       }
       if(result === 1) {
-        let lghicon = message.author.displayAvatarURL;
-        let lghembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let lghicon = message.author.displayAvatarURL();
+        let lghembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Laugh 😂", lghicon)
         .setDescription(`***${message.author.username} is laughing at ${rUser.user.username}.***\nWhat's so funny? 😅`)
         .setImage (`https://blusparkstudio.com/discord/laugh_2.gif`);
         return message.channel.send(lghembed);
       }
       if(result === 2) {
-        let lghicon = message.author.displayAvatarURL;
-        let lghembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let lghicon = message.author.displayAvatarURL();
+        let lghembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Laugh 😂", lghicon)
         .setDescription(`***${message.author.username} is laughing at ${rUser.user.username}.***\nWhat's so funny? 😅`)
         .setImage (`https://blusparkstudio.com/discord/laugh_3.gif`);
         return message.channel.send(lghembed);
       }
       if(result === 3) {
-        let lghicon = message.author.displayAvatarURL;
-        let lghembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let lghicon = message.author.displayAvatarURL();
+        let lghembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Laugh 😂", lghicon)
         .setDescription(`***${message.author.username} is laughing at ${rUser.user.username}.***\nWhat's so funny? 😅`)
         .setImage (`https://blusparkstudio.com/discord/laugh_4.gif`);
         return message.channel.send(lghembed);
       }
       if(result === 4) {
-        let lghicon = message.author.displayAvatarURL;
-        let lghembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let lghicon = message.author.displayAvatarURL();
+        let lghembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Laugh 😂", lghicon)
         .setDescription(`***${message.author.username} is laughing at ${rUser.user.username}.***\nWhat's so funny? 😅`)
         .setImage (`https://blusparkstudio.com/discord/laugh_5.gif`);
         return message.channel.send(lghembed);
       }
       if(result === 5) {
-        let lghicon = message.author.displayAvatarURL;
-        let lghembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let lghicon = message.author.displayAvatarURL();
+        let lghembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Laugh 😂", lghicon)
         .setDescription(`***${message.author.username} is laughing at ${rUser.user.username}.***\nWhat's so funny? 😅`)
         .setImage (`https://blusparkstudio.com/discord/laugh_6.gif`);
         return message.channel.send(lghembed);
       }
       if(result === 6) {
-        let lghicon = message.author.displayAvatarURL;
-        let lghembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let lghicon = message.author.displayAvatarURL();
+        let lghembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Laugh 😂", lghicon)
         .setDescription(`***${message.author.username} is laughing at ${rUser.user.username}.***\nWhat's so funny? 😅`)
         .setImage (`https://blusparkstudio.com/discord/laugh_7.gif`);
         return message.channel.send(lghembed);
       }
       if(result === 7) {
-        let lghicon = message.author.displayAvatarURL;
-        let lghembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let lghicon = message.author.displayAvatarURL();
+        let lghembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Laugh 😂", lghicon)
         .setDescription(`***${message.author.username} is laughing at ${rUser.user.username}.***\nWhat's so funny? 😅`)
         .setImage (`https://blusparkstudio.com/discord/laugh_8.gif`);
         return message.channel.send(lghembed);
       }
       if(result === 8) {
-        let lghicon = message.author.displayAvatarURL;
-        let lghembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let lghicon = message.author.displayAvatarURL();
+        let lghembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Laugh 😂", lghicon)
         .setDescription(`***${message.author.username} is laughing at ${rUser.user.username}.***\nWhat's so funny? 😅`)
         .setImage (`https://blusparkstudio.com/discord/laugh_9.gif`);
