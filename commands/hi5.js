f43e554b4550fa18958228a9495946f93c8cadbb
MattyG7 <ml.greaves1@btinternet.com>
2022-06-15 00:33:06 +0100

    Update hi5.js

--- commands/hi5.js
+++ commands/hi5.js
@@ -1,13 +1,11 @@
 const Discord = require("discord.js");
 const mongoose = require(`mongoose`);
-const Schema = mongoose.Schema;
 
 module.exports.run = async (bot, message, args) => {
   let rUser = message.mentions.members.first();
   if(!rUser) return message.channel.send(`User not found! 😕`);
   if(rUser.id === message.author.id) return message.channel.send(`***Did you just high five yourself, ${message.author.username}?***`);
 
-  //GET USER COLOUR
   mongoose.model("DiscordUserData").findOne ({
     userID: `${message.author.id}`
   }, function(error, data) {
@@ -15,120 +13,112 @@ module.exports.run = async (bot, message, args) => {
       console.log("Failed to get data :(");
       console.log(error);
     } else {
-      let userColour = "";
-      if (data.col === "not-set") {
-        userColour = "202225";
-      } else {
-        userColour = data.col;
-      }
-      console.log("Got user's colour Successfully!");
-
       let replies = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"];
       let result = Math.floor((Math.random() * replies.length));
 
       if(result === 0) {
-        let h5icon = message.author.displayAvatarURL;
-        let h5embed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let h5icon = message.author.displayAvatarURL();
+        let h5embed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("High Five ✋", h5icon)
         .setDescription(`***${message.author.username} high fived ${rUser.user.username}.***\n👏`)
         .setImage (`https://blusparkstudio.com/discord/hi5_1.gif`);
         return message.channel.send(h5embed);
       }
       if(result === 1) {
-        let h5icon = message.author.displayAvatarURL;
-        let h5embed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let h5icon = message.author.displayAvatarURL();
+        let h5embed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("High Five ✋", h5icon)
         .setDescription(`***${message.author.username} high fived ${rUser.user.username}.***\n👏`)
         .setImage (`https://blusparkstudio.com/discord/hi5_2.gif`);
         return message.channel.send(h5embed);
       }
       if(result === 2) {
-        let h5icon = message.author.displayAvatarURL;
-        let h5embed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let h5icon = message.author.displayAvatarURL();
+        let h5embed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("High Five ✋", h5icon)
         .setDescription(`***${message.author.username} high fived ${rUser.user.username}.***\n👏`)
         .setImage (`https://blusparkstudio.com/discord/hi5_3.gif`);
         return message.channel.send(h5embed);
       }
       if(result === 3) {
-        let h5icon = message.author.displayAvatarURL;
-        let h5embed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let h5icon = message.author.displayAvatarURL();
+        let h5embed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("High Five ✋", h5icon)
         .setDescription(`***${message.author.username} high fived ${rUser.user.username}.***\n👏`)
         .setImage (`https://blusparkstudio.com/discord/hi5_4.gif`);
         return message.channel.send(h5embed);
       }
       if(result === 4) {
-        let h5icon = message.author.displayAvatarURL;
-        let h5embed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let h5icon = message.author.displayAvatarURL();
+        let h5embed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("High Five ✋", h5icon)
         .setDescription(`***${message.author.username} high fived ${rUser.user.username}.***\n👏`)
         .setImage (`https://blusparkstudio.com/discord/hi5_5.gif`);
         return message.channel.send(h5embed);
       }
       if(result === 5) {
-        let h5icon = message.author.displayAvatarURL;
-        let h5embed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let h5icon = message.author.displayAvatarURL();
+        let h5embed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("High Five ✋", h5icon)
         .setDescription(`***${message.author.username} high fived ${rUser.user.username}.***\n👏`)
         .setImage (`https://blusparkstudio.com/discord/hi5_6.gif`);
         return message.channel.send(h5embed);
       }
       if(result === 6) {
-        let h5icon = message.author.displayAvatarURL;
-        let h5embed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let h5icon = message.author.displayAvatarURL();
+        let h5embed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("High Five ✋", h5icon)
         .setDescription(`***${message.author.username} high fived ${rUser.user.username}.***\n👏`)
         .setImage (`https://blusparkstudio.com/discord/hi5_7.gif`);
         return message.channel.send(h5embed);
       }
       if(result === 7) {
-        let h5icon = message.author.displayAvatarURL;
-        let h5embed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let h5icon = message.author.displayAvatarURL();
+        let h5embed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("High Five ✋", h5icon)
         .setDescription(`***${message.author.username} high fived ${rUser.user.username}.***\n👏`)
         .setImage (`https://blusparkstudio.com/discord/hi5_8.gif`);
         return message.channel.send(h5embed);
       }
       if(result === 8) {
-        let h5icon = message.author.displayAvatarURL;
-        let h5embed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let h5icon = message.author.displayAvatarURL();
+        let h5embed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("High Five ✋", h5icon)
         .setDescription(`***${message.author.username} high fived ${rUser.user.username}.***\n👏`)
         .setImage (`https://blusparkstudio.com/discord/hi5_9.gif`);
         return message.channel.send(h5embed);
       }
       if(result === 9) {
-        let h5icon = message.author.displayAvatarURL;
-        let h5embed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let h5icon = message.author.displayAvatarURL();
+        let h5embed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("High Five ✋", h5icon)
         .setDescription(`***${message.author.username} high fived ${rUser.user.username}.***\n👏`)
         .setImage (`https://blusparkstudio.com/discord/hi5_10.gif`);
         return message.channel.send(h5embed);
       }
       if(result === 10) {
-        let h5icon = message.author.displayAvatarURL;
-        let h5embed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let h5icon = message.author.displayAvatarURL();
+        let h5embed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("High Five ✋", h5icon)
         .setDescription(`***${message.author.username} high fived ${rUser.user.username}.***\n👏`)
         .setImage (`https://blusparkstudio.com/discord/hi5_11.gif`);
         return message.channel.send(h5embed);
       }
       if(result === 11) {
-        let h5icon = message.author.displayAvatarURL;
-        let h5embed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let h5icon = message.author.displayAvatarURL();
+        let h5embed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("High Five ✋", h5icon)
         .setDescription(`***${message.author.username} high fived ${rUser.user.username}.***\n👏`)
         .setImage (`https://blusparkstudio.com/discord/hi5_12.gif`);
