05cd7dbbf741eb2b3870ef13ab630b2408f93e3e
MattyG7 <ml.greaves1@btinternet.com>
2022-06-15 00:32:10 +0100

    Update helpx.js

--- commands/helpx.js
+++ commands/helpx.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  let helpxembed = new Discord.RichEmbed()
+  let helpxembed = new Discord.MessageEmbed()
   .setDescription("**Command Examples:**")
   .setColor("#1c9472")
   .addField("Profile Stuff", "`~lvl`** | **`~daily`** | **`~w`** | **`~p`** | **`~setup`, `~setup lo UK`** | **`~getdate`** | **`~getday`** | **`~getmonth`** | **`~getmyid`** | **`~getmynametag`** | **`~getmyrole`** | **`~getmyusername`")
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
   .addField("Games (no prize)", "`~flip`...")
   .addField("Games", "`~dontgetbit SPARKCOINAMOUNT`** | **`~flip COINSIDE SPARKCOINAMOUNT`** | **`~rps CHOICE SPARKCOINAMOUNT`")
   .addField("Daily Games", "`~bowl SPARKCOINAMOUNT`** | **`~deal low`, `~deal high`** | **`~memory`** | **`~penalty prize`** | **`~roll DICENUMBER`")
-  .addField("Fun", "`~8ball should i go to bed`** | **`~blumoji`** | **`~say i am a bot`")
+  .addField("Fun", "`~8ball should i go to bed`** | **`~blumoji`** | **`~c`** | **`~say i am a bot`")
   .addField("Randomising", "`~anime`** | **`~colour`** | **`~emoji`** | **`~fortune`** | **`~loli`** | **`~sing`")
   .addField("Education", "`~calc`")
   .addField("Admin Only", "`~clear MESSAGESAMOUNT`** | **`~gift @NAME SPARKCOINAMOUNT`** | **`~log USERNAME SPARKCOINAMOUNT GAMENAME`** | **`~spam MESSAGESAMOUNT`** | **`~steal @NAME SPARKCOINAMOUNT`")
