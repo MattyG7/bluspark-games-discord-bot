f80d0ba3585114d29272ce3e835902ab21c22409
MattyG7 <ml.greaves1@btinternet.com>
2022-06-15 00:47:55 +0100

    Update setup.js

--- commands/setup.js
+++ commands/setup.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const mongoose = require(`mongoose`);
-const Schema = mongoose.Schema;
 
 module.exports.run = async (bot, message, args) => {
   let setupUser = `${message.author.id}`;
@@ -11,21 +10,15 @@ module.exports.run = async (bot, message, args) => {
       console.log("Failed to get data :(");
       console.log(error);
     } else {
-      let col = "";
-      if (data.col === "not-set") {
-        col = "202225";
-      } else {
-        col = data.col;
-      }
       web = data.web;
       yt = data.yt;
       tw = data.tw;
       lo = data.lo;
       if(!args[0]) {
-        let setupicon = message.author.displayAvatarURL;
-        let setupembed = new Discord.RichEmbed()
+        let setupicon = message.author.displayAvatarURL();
+        let setupembed = new Discord.MessageEmbed()
         .setAuthor(`Profile Setup`, setupicon)
-        .setColor(`#${col}`)
+        .setColor(`${data.col}`)
         .addField("Profile Colour (use hex codes only and don't include the #)", `~setup col **6charhexcolour**`)
         .addField("Website (remove 'https://' and 'www.' from the url)", `~setup web **websiteurl**`)
         .addField("YouTube Channel (remove 'https://' and 'www.' from the url)", `~setup yt **youtubechannelurl**`)
@@ -34,7 +27,7 @@ module.exports.run = async (bot, message, args) => {
         return message.channel.send(setupembed);
       }
       if(args[0] === "col") {
-        if(!args[1]) return message.channel.send("Please specify a 6 character hex colour code. Confused? Search 'hex colour picker' on Google.");
+        if(!args[1]) return message.channel.send("Please specify a 6 character hex colour code. Confused? Search 'colour picker' on Google.");
         mongoose.model("DiscordUserData").updateOne ({userID: setupUser}, {
           col: `${args[1]}`
         }, function(error, data) {
@@ -44,7 +37,7 @@ module.exports.run = async (bot, message, args) => {
           } else {
             console.log(`Successfully updated user's profile colour!`);
             console.log(`- BEFORE: ${col}. AFTER: ${args[1]}`);
-            let setupcolembed = new Discord.RichEmbed()
+            let setupcolembed = new Discord.MessageEmbed()
             .setColor(`#${args[1]}`)
             .setDescription(`✅ ${message.author.username}'s new profile colour has successfully been set!`);
             return message.channel.send(setupcolembed);
@@ -62,15 +55,15 @@ module.exports.run = async (bot, message, args) => {
           } else {
             console.log(`Successfully updated user's website url!`);
             console.log(`- BEFORE: ${web}. AFTER: ${args[1]}`);
-            let setupcolembed = new Discord.RichEmbed()
-            .setColor(`#${col}`)
+            let setupcolembed = new Discord.MessageEmbed()
+            .setColor(`${data.col}`)
             .setDescription(`✅ ${message.author.username}'s website has successfully been set on their profile!`);
             return message.channel.send(setupcolembed);
           }
         });
       }
       if(args[0] === "yt") {
-        if(!args[1]) return message.channel.send("Please specify your YouTube channel url but remember to remove the 'https://' and 'www.'.");
+        if(!args[1]) return message.channel.send("Please specify your YouTube Channel URL but remember to remove the 'https://' and 'www.'.");
         mongoose.model("DiscordUserData").updateOne ({userID: setupUser}, {
           yt: `${args[1]}`
         }, function(error, data) {
@@ -78,10 +71,10 @@ module.exports.run = async (bot, message, args) => {
             console.log("Failed to save the data :(");
             console.log(error);
           } else {
-            console.log(`Successfully updated user's YouTube channel url!`);
+            console.log(`Successfully updated user's YouTube Channel URL!`);
             console.log(`- BEFORE: ${yt}. AFTER: ${args[1]}`);
-            let setupcolembed = new Discord.RichEmbed()
-            .setColor(`#${col}`)
+            let setupcolembed = new Discord.MessageEmbed()
+            .setColor(`${data.col}`)
             .setDescription(`✅ ${message.author.username}'s YouTube channel has successfully been set on their profile!`);
             return message.channel.send(setupcolembed);
           }
@@ -98,8 +91,8 @@ module.exports.run = async (bot, message, args) => {
           } else {
             console.log(`Successfully updated user's Twitter username!`);
             console.log(`- BEFORE: ${tw}. AFTER: ${args[1]}`);
-            let setupcolembed = new Discord.RichEmbed()
-            .setColor(`#${col}`)
+            let setupcolembed = new Discord.MessageEmbed()
+            .setColor(`${data.col}`)
             .setDescription(`✅ ${message.author.username}'s Twitter username has successfully been set on their profile!`);
             return message.channel.send(setupcolembed);
           }
@@ -108,7 +101,7 @@ module.exports.run = async (bot, message, args) => {
       if(args[0] === "lo") {
         if(!args[1]) {
           let replies = ["Limbo", "Time Vortex", "The Void", "The Sun", "Discord's server room", "Aether"];
-          let repliess = ["The Space Time Rift opened up and decided what your location shall be...", "LoCAtIon hAs BeEN seT..."];
+          let repliess = ["The Spacetime Rift opened up and decided what your location shall be...", "LoCAtIon hAs BeEN seT..."];
           let result = Math.floor((Math.random() * replies.length));
           let resultt = Math.floor((Math.random() * repliess.length));
           mongoose.model("DiscordUserData").updateOne ({userID: setupUser}, {
@@ -133,8 +126,8 @@ module.exports.run = async (bot, message, args) => {
             } else {
               console.log(`Successfully updated user's location!`);
               console.log(`- BEFORE: ${lo}. AFTER: ${args[1]}`);
-              let setupcolembed = new Discord.RichEmbed()
-              .setColor(`#${col}`)
+              let setupcolembed = new Discord.MessageEmbed()
+              .setColor(`${data.col}`)
               .setDescription(`✅ ${message.author.username}'s location has successfully been set on their profile!`);
               return message.channel.send(setupcolembed);
             }
