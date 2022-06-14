aba8e96917b8c194ab83996c3982b32d2b245200
MattyG7 <ml.greaves1@btinternet.com>
2022-06-15 00:51:26 +0100

    Update snog.js

--- commands/snog.js
+++ commands/snog.js
@@ -1,13 +1,11 @@
 const Discord = require("discord.js");
 const mongoose = require(`mongoose`);
-const Schema = mongoose.Schema;
 
 module.exports.run = async (bot, message, args) => {
   let rUser = message.mentions.members.first();
-  if(!rUser) return message.channel.send(`User not found! 😕`);
+  if(!rUser) return message.channel.send(`You can't snog no one...`);
   if(rUser.id === message.author.id) return message.channel.send(`***${message.author.username}, that just looks weird...***`);
 
-  //GET USER COLOUR
   mongoose.model("DiscordUserData").findOne ({
     userID: `${message.author.id}`
   }, function(error, data) {
@@ -15,93 +13,85 @@ module.exports.run = async (bot, message, args) => {
       console.log("Failed to get data :(");
       console.log(error);
     } else {
-      let userColour = "";
-      if (data.col === "not-set") {
-        userColour = "202225";
-      } else {
-        userColour = data.col;
-      }
-      console.log("Got user's colour Successfully!");
-
       let replies = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
       let result = Math.floor((Math.random() * replies.length));
 
       if(result === 0) {
-        let sgicon = message.author.displayAvatarURL;
-        let sgembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let sgicon = message.author.displayAvatarURL();
+        let sgembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Snog 💋", sgicon)
         .setDescription(`***${message.author.username} snogged ${rUser.user.username}.***\nGet a room! 🙈`)
         .setImage (`https://blusparkstudio.com/discord/snog_1.gif`);
         return message.channel.send(sgembed);
       }
       if(result === 1) {
-        let sgicon = message.author.displayAvatarURL;
-        let sgembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let sgicon = message.author.displayAvatarURL();
+        let sgembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Snog 💋", sgicon)
         .setDescription(`***${message.author.username} snogged ${rUser.user.username}.***\nGet a room! 🙈`)
         .setImage (`https://blusparkstudio.com/discord/snog_2.gif`);
         return message.channel.send(sgembed);
       }
       if(result === 2) {
-        let sgicon = message.author.displayAvatarURL;
-        let sgembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let sgicon = message.author.displayAvatarURL();
+        let sgembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Snog 💋", sgicon)
         .setDescription(`***${message.author.username} snogged ${rUser.user.username}.***\nGet a room! 🙈`)
         .setImage (`https://blusparkstudio.com/discord/snog_3.gif`);
         return message.channel.send(sgembed);
       }
       if(result === 3) {
-        let sgicon = message.author.displayAvatarURL;
-        let sgembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let sgicon = message.author.displayAvatarURL();
+        let sgembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Snog 💋", sgicon)
         .setDescription(`***${message.author.username} snogged ${rUser.user.username}.***\nGet a room! 🙈`)
         .setImage (`https://blusparkstudio.com/discord/snog_4.gif`);
         return message.channel.send(sgembed);
       }
       if(result === 4) {
-        let sgicon = message.author.displayAvatarURL;
-        let sgembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let sgicon = message.author.displayAvatarURL();
+        let sgembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Snog 💋", sgicon)
         .setDescription(`***${message.author.username} snogged ${rUser.user.username}.***\nGet a room! 🙈`)
         .setImage (`https://blusparkstudio.com/discord/snog_5.gif`);
         return message.channel.send(sgembed);
       }
       if(result === 5) {
-        let sgicon = message.author.displayAvatarURL;
-        let sgembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let sgicon = message.author.displayAvatarURL();
+        let sgembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Snog 💋", sgicon)
         .setDescription(`***${message.author.username} snogged ${rUser.user.username}.***\nGet a room! 🙈`)
         .setImage (`https://blusparkstudio.com/discord/snog_6.gif`);
         return message.channel.send(sgembed);
       }
       if(result === 6) {
-        let sgicon = message.author.displayAvatarURL;
-        let sgembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let sgicon = message.author.displayAvatarURL();
+        let sgembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Snog 💋", sgicon)
         .setDescription(`***${message.author.username} snogged ${rUser.user.username}.***\nGet a room! 🙈`)
         .setImage (`https://blusparkstudio.com/discord/snog_7.gif`);
         return message.channel.send(sgembed);
       }
       if(result === 7) {
-        let sgicon = message.author.displayAvatarURL;
-        let sgembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let sgicon = message.author.displayAvatarURL();
+        let sgembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Snog 💋", sgicon)
         .setDescription(`***${message.author.username} snogged ${rUser.user.username}.***\nGet a room! 🙈`)
         .setImage (`https://blusparkstudio.com/discord/snog_8.gif`);
         return message.channel.send(sgembed);
       }
       if(result === 8) {
-        let sgicon = message.author.displayAvatarURL;
-        let sgembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let sgicon = message.author.displayAvatarURL();
+        let sgembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Snog 💋", sgicon)
         .setDescription(`***${message.author.username} snogged ${rUser.user.username}.***\nGet a room! 🙈`)
         .setImage (`https://blusparkstudio.com/discord/snog_9.gif`);
