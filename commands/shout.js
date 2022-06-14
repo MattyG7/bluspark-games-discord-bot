376e07dca9fe453406fa441708e66b9b481f18de
MattyG7 <ml.greaves1@btinternet.com>
2022-06-15 00:48:08 +0100

    Update shout.js

--- commands/shout.js
+++ commands/shout.js
@@ -1,9 +1,7 @@
 const Discord = require("discord.js");
 const mongoose = require(`mongoose`);
-const Schema = mongoose.Schema;
 
 module.exports.run = async (bot, message, args) => {
-  //GET USER COLOUR
   mongoose.model("DiscordUserData").findOne ({
     userID: `${message.author.id}`
   }, function(error, data) {
@@ -11,118 +9,110 @@ module.exports.run = async (bot, message, args) => {
       console.log("Failed to get data :(");
       console.log(error);
     } else {
-      let userColour = "";
-      if (data.col === "not-set") {
-        userColour = "202225";
-      } else {
-        userColour = data.col;
-      }
-      console.log("Got user's colour Successfully!");
-
       if (!args[0]) {
         let replies = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
         let result = Math.floor((Math.random() * replies.length));
 
         if(result === 0) {
-          let shticon = message.author.displayAvatarURL;
-          let shtembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let shticon = message.author.displayAvatarURL();
+          let shtembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is shouting.`, shticon)
           .setImage (`https://blusparkstudio.com/discord/shout_1.gif`);
           return message.channel.send(shtembed);
         }
         if(result === 1) {
-          let shticon = message.author.displayAvatarURL;
-          let shtembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let shticon = message.author.displayAvatarURL();
+          let shtembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is shouting.`, shticon)
           .setImage (`https://blusparkstudio.com/discord/shout_2.gif`);
           return message.channel.send(shtembed);
         }
         if(result === 2) {
-          let shticon = message.author.displayAvatarURL;
-          let shtembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let shticon = message.author.displayAvatarURL();
+          let shtembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is shouting.`, shticon)
           .setImage (`https://blusparkstudio.com/discord/shout_3.gif`);
           return message.channel.send(shtembed);
         }
         if(result === 3) {
-          let shticon = message.author.displayAvatarURL;
-          let shtembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let shticon = message.author.displayAvatarURL();
+          let shtembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is shouting.`, shticon)
           .setImage (`https://blusparkstudio.com/discord/shout_4.gif`);
           return message.channel.send(shtembed);
         }
         if(result === 4) {
-          let shticon = message.author.displayAvatarURL;
-          let shtembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let shticon = message.author.displayAvatarURL();
+          let shtembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is shouting.`, shticon)
           .setImage (`https://blusparkstudio.com/discord/shout_5.gif`);
           return message.channel.send(shtembed);
         }
         if(result === 5) {
-          let shticon = message.author.displayAvatarURL;
-          let shtembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let shticon = message.author.displayAvatarURL();
+          let shtembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is shouting.`, shticon)
           .setImage (`https://blusparkstudio.com/discord/shout_6.gif`);
           return message.channel.send(shtembed);
         }
         if(result === 6) {
-          let shticon = message.author.displayAvatarURL;
-          let shtembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let shticon = message.author.displayAvatarURL();
+          let shtembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is shouting.`, shticon)
           .setImage (`https://blusparkstudio.com/discord/shout_7.gif`);
           return message.channel.send(shtembed);
         }
         if(result === 7) {
-          let shticon = message.author.displayAvatarURL;
-          let shtembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let shticon = message.author.displayAvatarURL();
+          let shtembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is shouting.`, shticon)
           .setImage (`https://blusparkstudio.com/discord/shout_8.gif`);
           return message.channel.send(shtembed);
         }
         if(result === 8) {
-          let shticon = message.author.displayAvatarURL;
-          let shtembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let shticon = message.author.displayAvatarURL();
+          let shtembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is shouting.`, shticon)
           .setImage (`https://blusparkstudio.com/discord/shout_9.gif`);
           return message.channel.send(shtembed);
         }
         if(result === 9) {
-          let shticon = message.author.displayAvatarURL;
-          let shtembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let shticon = message.author.displayAvatarURL();
+          let shtembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is shouting.`, shticon)
           .setImage (`https://blusparkstudio.com/discord/shout_10.gif`);
           return message.channel.send(shtembed);
         }
         if(result === 10) {
-          let shticon = message.author.displayAvatarURL;
-          let shtembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let shticon = message.author.displayAvatarURL();
+          let shtembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is shouting.`, shticon)
           .setImage (`https://blusparkstudio.com/discord/shout_11.gif`);
           return message.channel.send(shtembed);
         }
         if(result === 11) {
-          let shticon = message.author.displayAvatarURL;
-          let shtembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let shticon = message.author.displayAvatarURL();
+          let shtembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is shouting.`, shticon)
           .setImage (`https://blusparkstudio.com/discord/shout_12.gif`);
           return message.channel.send(shtembed);
         }
         if(result === 12) {
-          let shticon = message.author.displayAvatarURL;
-          let shtembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let shticon = message.author.displayAvatarURL();
+          let shtembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is shouting.`, shticon)
           .setImage (`https://blusparkstudio.com/discord/shout_13.gif`);
           return message.channel.send(shtembed);
@@ -136,117 +126,117 @@ module.exports.run = async (bot, message, args) => {
       let result = Math.floor((Math.random() * replies.length));
 
       if(result === 0) {
-        let shticon = message.author.displayAvatarURL;
-        let shtembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let shticon = message.author.displayAvatarURL();
+        let shtembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Shout 📢", shticon)
         .setDescription(`***${message.author.username} is shouting at ${rUser.user.username}.***‍\nGlad I'm not the one being scolded. 🙈`)
         .setImage (`https://blusparkstudio.com/discord/shout_1.gif`);
         return message.channel.send(shtembed);
       }
       if(result === 1) {
-        let shticon = message.author.displayAvatarURL;
-        let shtembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let shticon = message.author.displayAvatarURL();
+        let shtembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Shout 📢", shticon)
         .setDescription(`***${message.author.username} is shouting at ${rUser.user.username}.***‍\nGlad I'm not the one being scolded. 🙈`)
         .setImage (`https://blusparkstudio.com/discord/shout_2.gif`);
         return message.channel.send(shtembed);
       }
       if(result === 2) {
-        let shticon = message.author.displayAvatarURL;
-        let shtembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let shticon = message.author.displayAvatarURL();
+        let shtembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Shout 📢", shticon)
         .setDescription(`***${message.author.username} is shouting at ${rUser.user.username}.***‍\nGlad I'm not the one being scolded. 🙈`)
         .setImage (`https://blusparkstudio.com/discord/shout_3.gif`);
         return message.channel.send(shtembed);
       }
       if(result === 3) {
-        let shticon = message.author.displayAvatarURL;
-        let shtembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let shticon = message.author.displayAvatarURL();
+        let shtembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Shout 📢", shticon)
         .setDescription(`***${message.author.username} is shouting at ${rUser.user.username}.***‍\nGlad I'm not the one being scolded. 🙈`)
         .setImage (`https://blusparkstudio.com/discord/shout_4.gif`);
         return message.channel.send(shtembed);
       }
       if(result === 4) {
-        let shticon = message.author.displayAvatarURL;
-        let shtembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let shticon = message.author.displayAvatarURL();
+        let shtembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Shout 📢", shticon)
         .setDescription(`***${message.author.username} is shouting at ${rUser.user.username}.***‍\nGlad I'm not the one being scolded. 🙈`)
         .setImage (`https://blusparkstudio.com/discord/shout_5.gif`);
         return message.channel.send(shtembed);
       }
       if(result === 5) {
-        let shticon = message.author.displayAvatarURL;
-        let shtembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let shticon = message.author.displayAvatarURL();
+        let shtembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Shout 📢", shticon)
         .setDescription(`***${message.author.username} is shouting at ${rUser.user.username}.***‍\nGlad I'm not the one being scolded. 🙈`)
         .setImage (`https://blusparkstudio.com/discord/shout_6.gif`);
         return message.channel.send(shtembed);
       }
       if(result === 6) {
-        let shticon = message.author.displayAvatarURL;
-        let shtembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let shticon = message.author.displayAvatarURL();
+        let shtembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Shout 📢", shticon)
         .setDescription(`***${message.author.username} is shouting at ${rUser.user.username}.***‍\nGlad I'm not the one being scolded. 🙈`)
         .setImage (`https://blusparkstudio.com/discord/shout_7.gif`);
         return message.channel.send(shtembed);
       }
       if(result === 7) {
-        let shticon = message.author.displayAvatarURL;
-        let shtembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let shticon = message.author.displayAvatarURL();
+        let shtembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Shout 📢", shticon)
         .setDescription(`***${message.author.username} is shouting at ${rUser.user.username}.***‍\nGlad I'm not the one being scolded. 🙈`)
         .setImage (`https://blusparkstudio.com/discord/shout_8.gif`);
         return message.channel.send(shtembed);
       }
       if(result === 8) {
-        let shticon = message.author.displayAvatarURL;
-        let shtembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let shticon = message.author.displayAvatarURL();
+        let shtembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Shout 📢", shticon)
         .setDescription(`***${message.author.username} is shouting at ${rUser.user.username}.***‍\nGlad I'm not the one being scolded. 🙈`)
         .setImage (`https://blusparkstudio.com/discord/shout_9.gif`);
         return message.channel.send(shtembed);
       }
       if(result === 9) {
-        let shticon = message.author.displayAvatarURL;
-        let shtembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let shticon = message.author.displayAvatarURL();
+        let shtembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Shout 📢", shticon)
         .setDescription(`***${message.author.username} is shouting at ${rUser.user.username}.***‍\nGlad I'm not the one being scolded. 🙈`)
         .setImage (`https://blusparkstudio.com/discord/shout_10.gif`);
         return message.channel.send(shtembed);
       }
       if(result === 10) {
-        let shticon = message.author.displayAvatarURL;
-        let shtembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let shticon = message.author.displayAvatarURL();
+        let shtembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Shout 📢", shticon)
         .setDescription(`***${message.author.username} is shouting at ${rUser.user.username}.***‍\nGlad I'm not the one being scolded. 🙈`)
         .setImage (`https://blusparkstudio.com/discord/shout_11.gif`);
         return message.channel.send(shtembed);
       }
       if(result === 11) {
-        let shticon = message.author.displayAvatarURL;
-        let shtembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let shticon = message.author.displayAvatarURL();
+        let shtembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Shout 📢", shticon)
         .setDescription(`***${message.author.username} is shouting at ${rUser.user.username}.***‍\nGlad I'm not the one being scolded. 🙈`)
         .setImage (`https://blusparkstudio.com/discord/shout_12.gif`);
         return message.channel.send(shtembed);
       }
       if(result === 12) {
-        let shticon = message.author.displayAvatarURL;
-        let shtembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let shticon = message.author.displayAvatarURL();
+        let shtembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Shout 📢", shticon)
         .setDescription(`***${message.author.username} is shouting at ${rUser.user.username}.***‍\nGlad I'm not the one being scolded. 🙈`)
         .setImage (`https://blusparkstudio.com/discord/shout_13.gif`);
