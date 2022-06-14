ffc4ae32334fed085778dc893eb75911f5e0be6a
MattyG7 <ml.greaves1@btinternet.com>
2022-06-15 00:29:13 +0100

    Update go.js

--- commands/go.js
+++ commands/go.js
@@ -1,9 +1,7 @@
 const Discord = require("discord.js");
 const mongoose = require(`mongoose`);
-const Schema = mongoose.Schema;
 
 module.exports.run = async (bot, message, args) => {
-  //GET USER COLOUR
   mongoose.model("DiscordUserData").findOne ({
     userID: `${message.author.id}`
   }, function(error, data) {
@@ -11,54 +9,46 @@ module.exports.run = async (bot, message, args) => {
       console.log("Failed to get data :(");
       console.log(error);
     } else {
-      let userColour = "";
-      if (data.col === "not-set") {
-        userColour = "202225";
-      } else {
-        userColour = data.col;
-      }
-      console.log("Got user's colour Successfully!");
-
       if (!args[0]) {
         let replies = ["0", "1", "2", "3", "4"];
         let result = Math.floor((Math.random() * replies.length));
 
         if(result === 0) {
-          let gicon = message.author.displayAvatarURL;
-          let gembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let gicon = message.author.displayAvatarURL();
+          let gembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} shouted "Go!".`, gicon)
           .setImage (`https://blusparkstudio.com/discord/go_1.gif`);
           return message.channel.send(gembed);
         }
         if(result === 1) {
-          let gicon = message.author.displayAvatarURL;
-          let gembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let gicon = message.author.displayAvatarURL();
+          let gembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is cheering "Go".`, gicon)
           .setImage (`https://blusparkstudio.com/discord/go_2.gif`);
           return message.channel.send(gembed);
         }
         if(result === 2) {
-          let gicon = message.author.displayAvatarURL;
-          let gembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let gicon = message.author.displayAvatarURL();
+          let gembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} is cheering to go.`, gicon)
           .setImage (`https://blusparkstudio.com/discord/go_3.gif`);
           return message.channel.send(gembed);
         }
         if(result === 3) {
-          let gicon = message.author.displayAvatarURL;
-          let gembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let gicon = message.author.displayAvatarURL();
+          let gembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} says "Go Pokemon!".`, gicon)
           .setImage (`https://blusparkstudio.com/discord/go_4.gif`);
           return message.channel.send(gembed);
         }
         if(result === 4) {
-          let gicon = message.author.displayAvatarURL;
-          let gembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let gicon = message.author.displayAvatarURL();
+          let gembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           .setAuthor(`${message.author.username} wants to get going!`, gicon)
           .setImage (`https://blusparkstudio.com/discord/go_5.gif`);
           return message.channel.send(gembed);
@@ -72,45 +62,45 @@ module.exports.run = async (bot, message, args) => {
       let result = Math.floor((Math.random() * replies.length));
 
       if(result === 0) {
-        let gicon = message.author.displayAvatarURL;
-        let gembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let gicon = message.author.displayAvatarURL();
+        let gembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Go 👉", gicon)
         .setDescription(`***${message.author.username} shouted "Go!" at ${rUser.user.username}.***\nI think you should go and leave them alone for a bit. 🙈`)
         .setImage (`https://blusparkstudio.com/discord/go_1.gif`);
         return message.channel.send(gembed);
       }
       if(result === 1) {
-        let gicon = message.author.displayAvatarURL;
-        let gembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let gicon = message.author.displayAvatarURL();
+        let gembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Go 👉", gicon)
         .setDescription(`***${message.author.username}is cheering "Go" at ${rUser.user.username}.***\nLet's go! 🚶‍`)
         .setImage (`https://blusparkstudio.com/discord/go_2.gif`);
         return message.channel.send(gembed);
       }
       if(result === 2) {
-        let gicon = message.author.displayAvatarURL;
-        let gembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let gicon = message.author.displayAvatarURL();
+        let gembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Go 👉", gicon)
         .setDescription(`***${message.author.username} is cheering to go with ${rUser.user.username}.***\nLet's go! 🚶`)
         .setImage (`https://blusparkstudio.com/discord/go_3.gif`);
         return message.channel.send(gembed);
       }
       if(result === 3) {
-        let gicon = message.author.displayAvatarURL;
-        let gembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let gicon = message.author.displayAvatarURL();
+        let gembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Go 👉", gicon)
         .setDescription(`***${message.author.username} threw a Pokeball with ${rUser.user.username} in.***\nGo ${rUser.user.username}, I choose you! 🐢`)
         .setImage (`https://blusparkstudio.com/discord/go_4.gif`);
         return message.channel.send(gembed);
       }
       if(result === 4) {
-        let gicon = message.author.displayAvatarURL;
-        let gembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let gicon = message.author.displayAvatarURL();
+        let gembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Go 👉", gicon)
         .setDescription(`***${message.author.username} is dragging ${rUser.user.username} along becasue they want to go.***\nCome on, fgs! Ugh. 🙄`)
         .setImage (`https://blusparkstudio.com/discord/go_5.gif`);
