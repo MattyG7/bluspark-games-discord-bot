c06c8d1e0036e5c19f277a49d77d4c9fb4bb33a9
MattyG7 <ml.greaves1@btinternet.com>
2022-06-15 00:39:18 +0100

    Update loli.js

--- commands/loli.js
+++ commands/loli.js
@@ -1,9 +1,7 @@
 const Discord = require("discord.js");
 const mongoose = require(`mongoose`);
-const Schema = mongoose.Schema;
 
 module.exports.run = async (bot, message, args) => {
-  //GET USER COLOUR
   mongoose.model("DiscordUserData").findOne ({
     userID: `${message.author.id}`
   }, function(error, data) {
@@ -11,62 +9,54 @@ module.exports.run = async (bot, message, args) => {
       console.log("Failed to get data :(");
       console.log(error);
     } else {
-      let userColour = "";
-      if (data.col === "not-set") {
-        userColour = "202225";
-      } else {
-        userColour = data.col;
-      }
-      console.log("Got user's colour Successfully!");
-
       if(!args[0]) {
         let replies = ["0", "1", "2", "3", "4", "5"];
         let result = Math.floor((Math.random() * replies.length));
 
         if(result === 0) {
-          let loliicon = message.author.displayAvatarURL;
-          let loliembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let loliicon = message.author.displayAvatarURL();
+          let loliembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           //.setAuthor(`A loli a day keps the doctor away`, loliicon)
           .setImage (`https://blusparkstudio.com/discord/loli_1.gif`);
           return message.channel.send(loliembed);
         }
         if(result === 1) {
-          let loliicon = message.author.displayAvatarURL;
-          let loliembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let loliicon = message.author.displayAvatarURL();
+          let loliembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           //.setAuthor(`A loli a day keps the doctor away`, loliicon)
           .setImage (`https://blusparkstudio.com/discord/loli_2.gif`);
           return message.channel.send(loliembed);
         }
         if(result === 2) {
-          let loliicon = message.author.displayAvatarURL;
-          let loliembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let loliicon = message.author.displayAvatarURL();
+          let loliembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           //.setAuthor(`A loli a day keps the doctor away`, loliicon)
           .setImage (`https://blusparkstudio.com/discord/loli_3.gif`);
           return message.channel.send(loliembed);
         }
         if(result === 3) {
-          let loliicon = message.author.displayAvatarURL;
-          let loliembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let loliicon = message.author.displayAvatarURL();
+          let loliembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           //.setAuthor(`A loli a day keps the doctor away`, loliicon)
           .setImage (`https://blusparkstudio.com/discord/loli_4.gif`);
           return message.channel.send(loliembed);
         }
         if(result === 4) {
-          let loliicon = message.author.displayAvatarURL;
-          let loliembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let loliicon = message.author.displayAvatarURL();
+          let loliembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           //.setAuthor(`A loli a day keps the doctor away`, loliicon)
           .setImage (`https://blusparkstudio.com/discord/loli_5.gif`);
           return message.channel.send(loliembed);
         }
         if(result === 5) {
-          let loliicon = message.author.displayAvatarURL;
-          let loliembed = new Discord.RichEmbed()
-          .setColor(`#${userColour}`)
+          let loliicon = message.author.displayAvatarURL();
+          let loliembed = new Discord.MessageEmbed()
+          .setColor(`${data.col}`)
           //.setAuthor(`A loli a day keps the doctor away`, loliicon)
           .setImage (`https://blusparkstudio.com/discord/loli_8.gif`);
           return message.channel.send(loliembed);
@@ -80,108 +70,108 @@ module.exports.run = async (bot, message, args) => {
       let result = Math.floor((Math.random() * replies.length));
 
       if(result === 0) {
-        let loliicon = message.author.displayAvatarURL;
-        let loliembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let loliicon = message.author.displayAvatarURL();
+        let loliembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Loli 👧", loliicon)
         .setDescription(`***${message.author.username} says that ${rUser.user.username} likes lolis.***\n❤`)
         .setImage (`https://blusparkstudio.com/discord/loli_1.gif`);
         return message.channel.send(loliembed);
       }
       if(result === 1) {
-        let loliicon = message.author.displayAvatarURL;
-        let loliembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let loliicon = message.author.displayAvatarURL();
+        let loliembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Loli 👧", loliicon)
         .setDescription(`***${message.author.username} says that ${rUser.user.username} likes lolis.***\nI dare ya to go sit with her.`)
         .setImage (`https://blusparkstudio.com/discord/loli_2.gif`);
         return message.channel.send(loliembed);
       }
       if(result === 2) {
-        let loliicon = message.author.displayAvatarURL;
-        let loliembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let loliicon = message.author.displayAvatarURL();
+        let loliembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Loli 👧", loliicon)
         .setDescription(`***${message.author.username} says that ${rUser.user.username} likes lolis.***\nNom nom. 😋`)
         .setImage (`https://blusparkstudio.com/discord/loli_3.gif`);
         return message.channel.send(loliembed);
       }
       if(result === 3) {
-        let loliicon = message.author.displayAvatarURL;
-        let loliembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let loliicon = message.author.displayAvatarURL();
+        let loliembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Loli 👧", loliicon)
         .setDescription(`***${message.author.username} says that ${rUser.user.username} likes lolis.***\n❤`)
         .setImage (`https://blusparkstudio.com/discord/loli_4.gif`);
         return message.channel.send(loliembed);
       }
       if(result === 4) {
-        let loliicon = message.author.displayAvatarURL;
-        let loliembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let loliicon = message.author.displayAvatarURL();
+        let loliembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Loli 👧", loliicon)
         .setDescription(`***${rUser.user.username} is a loli.***\n😜`)
         .setImage (`https://blusparkstudio.com/discord/loli_1.gif`);
         return message.channel.send(loliembed);
       }
       if(result === 5) {
-        let loliicon = message.author.displayAvatarURL;
-        let loliembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let loliicon = message.author.displayAvatarURL();
+        let loliembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Loli 👧", loliicon)
         .setDescription(`***${rUser.user.username} is a loli.***\n😜`)
         .setImage (`https://blusparkstudio.com/discord/loli_2.gif`);
         return message.channel.send(loliembed);
       }
       if(result === 6) {
-        let loliicon = message.author.displayAvatarURL;
-        let loliembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let loliicon = message.author.displayAvatarURL();
+        let loliembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Loli 👧", loliicon)
         .setDescription(`***${rUser.user.username} is a loli.***\n😜`)
         .setImage (`https://blusparkstudio.com/discord/loli_3.gif`);
         return message.channel.send(loliembed);
       }
       if(result === 7) {
-        let loliicon = message.author.displayAvatarURL;
-        let loliembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let loliicon = message.author.displayAvatarURL();
+        let loliembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Loli 👧", loliicon)
         .setDescription(`***${rUser.user.username} is a loli.***\n😜`)
         .setImage (`https://blusparkstudio.com/discord/loli_4.gif`);
         return message.channel.send(loliembed);
       }
       if(result === 8) {
-        let loliicon = message.author.displayAvatarURL;
-        let loliembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let loliicon = message.author.displayAvatarURL();
+        let loliembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Loli 👧", loliicon)
         .setDescription(`***${rUser.user.username} is a loli...with a whale.***\n🐋`)
         .setImage (`https://blusparkstudio.com/discord/loli_5.gif`);
         return message.channel.send(loliembed);
       }
       if(result === 9) {
-        let loliicon = message.author.displayAvatarURL;
-        let loliembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let loliicon = message.author.displayAvatarURL();
+        let loliembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Loli 👧", loliicon)
         .setDescription(`***${rUser.user.username} is a sad loli.***\n😢`)
         .setImage (`https://blusparkstudio.com/discord/loli_6.gif`);
         return message.channel.send(loliembed);
       }
       if(result === 10) {
-        let loliicon = message.author.displayAvatarURL;
-        let loliembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let loliicon = message.author.displayAvatarURL();
+        let loliembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Loli 👧", loliicon)
         .setDescription(`***${rUser.user.username} is a hungry loli.***\n🍪`)
         .setImage (`https://blusparkstudio.com/discord/loli_7.gif`);
         return message.channel.send(loliembed);
       }
       if(result === 11) {
-        let loliicon = message.author.displayAvatarURL;
-        let loliembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let loliicon = message.author.displayAvatarURL();
+        let loliembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Loli 👧", loliicon)
         .setDescription(`***${rUser.user.username} is a hungry loli.***\n🍪`)
         .setImage (`https://blusparkstudio.com/discord/loli_8.gif`);
