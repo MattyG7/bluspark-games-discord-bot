fa4b5934c9fe658763b2f9ec963bed352eec61a7
MattyG7 <ml.greaves1@btinternet.com>
2022-06-15 00:36:52 +0100

    Update kill.js

--- commands/kill.js
+++ commands/kill.js
@@ -1,13 +1,11 @@
 const Discord = require("discord.js");
 const mongoose = require(`mongoose`);
-const Schema = mongoose.Schema;
 
 module.exports.run = async (bot, message, args) => {
   let rUser = message.mentions.members.first();
   if(!rUser) return message.channel.send(`User not found! 😕`);
   if(rUser.id === message.author.id) return message.channel.send(`***You can't do that, ${message.author.username}!***`);
 
-  //GET USER COLOUR
   mongoose.model("DiscordUserData").findOne ({
     userID: `${message.author.id}`
   }, function(error, data) {
@@ -15,66 +13,58 @@ module.exports.run = async (bot, message, args) => {
       console.log("Failed to get data :(");
       console.log(error);
     } else {
-      let userColour = "";
-      if (data.col === "not-set") {
-        userColour = "202225";
-      } else {
-        userColour = data.col;
-      }
-      console.log("Got user's colour Successfully!");
-
       let replies = ["0", "1", "2", "3", "4", "5"];
       let result = Math.floor((Math.random() * replies.length));
 
       if(result === 0) {
-        let kllicon = message.author.displayAvatarURL;
-        let kllembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let kllicon = message.author.displayAvatarURL();
+        let kllembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Kill 👼", kllicon)
         .setDescription(`***${rUser.user.username} got wasted!***\n... 😨`)
         .setImage (`https://blusparkstudio.com/discord/kill_1.gif`);
         return message.channel.send(kllembed);
       }
       if(result === 1) {
-        let kllicon = message.author.displayAvatarURL;
-        let kllembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let kllicon = message.author.displayAvatarURL();
+        let kllembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Kill 👼", kllicon)
         .setDescription(`***${rUser.user.username} got wasted!***\n... 😨`)
         .setImage (`https://blusparkstudio.com/discord/kill_2.gif`);
         return message.channel.send(kllembed);
       }
       if(result === 2) {
-        let kllicon = message.author.displayAvatarURL;
-        let kllembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let kllicon = message.author.displayAvatarURL();
+        let kllembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Kill 👼", kllicon)
         .setDescription(`***${rUser.user.username} got wasted!***\n... 😨`)
         .setImage (`https://blusparkstudio.com/discord/kill_3.gif`);
         return message.channel.send(kllembed);
       }
       if(result === 3) {
-        let kllicon = message.author.displayAvatarURL;
-        let kllembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let kllicon = message.author.displayAvatarURL();
+        let kllembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Kill 👼", kllicon)
         .setDescription(`***${rUser.user.username} got wasted!***\n... 😨`)
         .setImage (`https://blusparkstudio.com/discord/kill_4.gif`);
         return message.channel.send(kllembed);
       }
       if(result === 4) {
-        let kllicon = message.author.displayAvatarURL;
-        let kllembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let kllicon = message.author.displayAvatarURL();
+        let kllembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Kill 👼", kllicon)
         .setDescription(`***${rUser.user.username} got wasted!***\n... 😨`)
         .setImage (`https://blusparkstudio.com/discord/kill_5.gif`);
         return message.channel.send(kllembed);
       }
       if(result === 5) {
-        let kllicon = message.author.displayAvatarURL;
-        let kllembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let kllicon = message.author.displayAvatarURL();
+        let kllembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setAuthor("Kill 👼", kllicon)
         .setDescription(`***${rUser.user.username} got wasted!***\n... 😨`)
         .setImage (`https://blusparkstudio.com/discord/kill_6.gif`);
