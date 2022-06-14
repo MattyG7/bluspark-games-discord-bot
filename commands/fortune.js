82326b14679297bec56a46a71dadec4bb81a248e
MattyG7 <ml.greaves1@btinternet.com>
2022-06-15 00:25:58 +0100

    Update fortune.js

--- commands/fortune.js
+++ commands/fortune.js
@@ -1,9 +1,7 @@
 const Discord = require("discord.js");
 const mongoose = require(`mongoose`);
-const Schema = mongoose.Schema;
 
 module.exports.run = async (bot, message, args, messageArray) => {
-  //GET USER COLOUR
   mongoose.model("DiscordUserData").findOne ({
     userID: `${message.author.id}`
   }, function(error, data) {
@@ -11,20 +9,12 @@ module.exports.run = async (bot, message, args, messageArray) => {
       console.log("Failed to get data :(");
       console.log(error);
     } else {
-      let userColour = "";
-      if (data.col === "not-set") {
-        userColour = "202225";
-      } else {
-        userColour = data.col;
-      }
-      console.log("Got user's colour Successfully!");
-
       if(!args[0]) {
         let replies = ["Today will be a good day.", "Today will be a bad day.", "Today will be an average day.", "Your future looks bright.", "Someone new will come along very soon.", "This'll be a good year.", "Things look good.", "Try again later, I can't forsee your future right now.", "The crystal ball is foggy, I can't forsee your future right now."];
         let result = Math.floor((Math.random() * replies.length));
 
-        let frtnembed = new Discord.RichEmbed()
-        .setColor(`#${userColour}`)
+        let frtnembed = new Discord.MessageEmbed()
+        .setColor(`${data.col}`)
         .setTitle(`*🔮 Fortune*`)
         .setDescription(`**${replies[result]}**`);
         return message.channel.send(frtnembed);
