1d64ece41d232836b4e43dffdf9de81872e161ec
MattyG7 <ml.greaves1@btinternet.com>
2022-06-16 13:27:04 +0100

    Add files via upload

---
+++ commands/coin.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const mongoose = require(`mongoose`);
+
+module.exports.run = async (bot, message, args) => {
+  mongoose.model("DiscordUserData").findOne ({
+    userID: `${message.author.id}`
+  }, function(error, data) {
+    if (error) {
+      console.log("Failed to get data :(");
+      console.log(error);
+    } else {
+      console.log("Successfully got the user's SparkCoin amount: " + data.sparkcoins);
+      let wicon = message.author.displayAvatarURL();
+      let wlltembed = new Discord.MessageEmbed()
+      .setColor(`${data.col}`)
+      .setAuthor(`💳 ${message.author.username}'s Wallet`, wicon)
+      .setDescription(`You have 0 SparkCoins.`)
+      .setFooter("Lol, jk.");
+      return message.channel.send(wlltembed);
+    }
+  });
+}
+
+module.exports.help = {
+  name: "coin"
+}
