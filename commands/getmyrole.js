661dcc5e2e5f63c5793545043f8f0b0d69be07b1
MattyG7 <ml.greaves1@btinternet.com>
2022-06-18 19:44:21 +0100

    Update getmyrole.js

--- commands/getmyrole.js
+++ commands/getmyrole.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  console.log(`Got user's role Successfully: ${memberRole}`);
   let memberRole = message.member.roles.cache.map(roles => roles.name).slice(0,-1);
+  console.log(`Got user's role Successfully: ${memberRole}`);
   return message.channel.send(`Your role is **${memberRole}**.`);
 }
 
