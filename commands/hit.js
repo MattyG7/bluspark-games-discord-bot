bc0bda05f106940c0c35471c70d2ddfd466d56f7
MattyG7 <ml.greaves1@btinternet.com>
2022-06-18 21:03:55 +0100

    Update hit.js

--- commands/hit.js
+++ commands/hit.js
@@ -3,7 +3,7 @@ const mongoose = require(`mongoose`);
 
 module.exports.run = async (bot, message, args) => {
   let rUser = message.mentions.members.first();
-  if(!rUser) return message.channel.send(`User not found! 😕`);
+  if(!rUser) return message.channel.send(`Be sure to mention someone you'd like to hit. 😕`);
   if(rUser.id === message.author.id) return message.channel.send(`***Don't hit yourself, ${message.author.username}!***`);
 
   mongoose.model("DiscordUserData").findOne ({
